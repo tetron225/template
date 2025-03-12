8299c30131cdc0c24bceef63e77dbcd8f236fe11
tetron <skim6502@gmail.com>
2025-03-12 09:00:21 -0400

    added template format for all projects

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const { mainModule } = require("process");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+}
